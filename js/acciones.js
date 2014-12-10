ea172921647676efa877c17f12b68f5a3d008c59
alumno135gcb16 <emmanuel.emma@gmail.com>
2014-12-10 13:34:53 -0600

    version4
    version4

--- js/acciones.js
+++ js/acciones.js
@@ -8,7 +8,7 @@ $(document).ready(function(e) {
 		}
 		
 		function Comienza() {
-			var opciones = {frequency:2000};
+			var opciones = { frequency:2000 };
 			
 			watchID = navigator.accelerometer.watchAcceleration(Correcto, Error, opciones);
 			navigator.geolocation.getCurrentPosition(Localiza, ErrorLocalizacion);
@@ -24,9 +24,9 @@ $(document).ready(function(e) {
 				
 				function Correcto(acceleration){
 					var element = document.getElementById('acelerometro');
-					element.innerHTML = 'Aceleracion en X: ' + acceleratio.x + '<br/>'+ 
-					'Aceleracion en Y: ' + acceleratio.y +' <br/>'+
-					'Aceleracion en Z: ' + acceleratio.z +' <br/>'+
+					element.innerHTML = 'Aceleracion en X: ' + acceleratio.x + '<br/>' + 
+					'Aceleracion en Y: ' + acceleratio.y + ' <br/>' +
+					'Aceleracion en Z: ' + acceleratio.z + ' <br/>' +
 					'Intervalo: ' + acceleration.timestamp +'<br/>';
 					
 					}
@@ -36,20 +36,20 @@ $(document).ready(function(e) {
 						
 						function Localiza(posicion){
 							
-							var element=document.getElementById('geolocalizacion');
-							element.innerHTML='Latitud: '+ posicion.coords.latitude +'<br/>'+
-							'Longitud: '+ posicion.coords.longitude +'<br/>'+
-							'Altitud: '+ posicion.coords.altitude +'<br/>'+
-							'Precision: '+ posicion.coords.accuracy +'<br/>'+
-							'Precision de Altitud: '+ posicion.coords.altitudeAccuracy +'<br/>'+
-							'Direccion: '+ posicion.coords.heading +'<br/>'+
-							'Velocidad: '+ posicion.coords.speed +'<br/>'+
-							'Intervalo: '+ posicion.timestamp +' <br/> ';
+							var element = document.getElementById('geolocalizacion');
+							element.innerHTML = 'Latitud: '+ posicion.coords.latitude +'<br/>' +
+							'Longitud: ' + posicion.coords.longitude + '<br/>' +
+							'Altitud: ' + posicion.coords.altitude + '<br/>' +
+							'Precision: ' + posicion.coords.accuracy + '<br/>' +
+							'Precision de Altitud: ' + posicion.coords.altitudeAccuracy +'<br/>' +
+							'Direccion: ' + posicion.coords.heading + '<br/>' +
+							'Velocidad: ' + posicion.coords.speed + '<br/>' +
+							'Intervalo: ' + posicion.timestamp + '<br/>';
 							}
 							
 							function ErrorLocalizacion(error){
-								alert('codigo: '+ error.code +'\n' +
-								'mensaje: '+ error.message +'\n');
+								alert('codigo: '+ error.code + '\n' +
+								'mensaje: '+ error.message + '\n');
 								
 								}
 														
